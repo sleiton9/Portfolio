5b6cdc03a1ac8b8b889738c3a247afc6508194bb
Sebastian Leiton <sebas_garzon@hotmail.com>
2020-03-19 22:48:28 -0500

    publicar pagina

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Mameluco.
+          Edit <code>src/App.js</code> and Mameluquin.
         </p>
         <a
           className="App-link"
