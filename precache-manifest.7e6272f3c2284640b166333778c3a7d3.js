fa023a742170de92839a07620182f04e8c5c8640
Sebastian Garzon Leiton <sebas_garzon@hotmail.com>
2020-03-19 23:29:16 -0500

    Updates

---
+++ precache-manifest.7e6272f3c2284640b166333778c3a7d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe546b304f070ad5e9979a5146b44d2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "64ae6936a3ef8a7831f8",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "352ae0281124255c909a",
+    "url": "/portfolio/static/css/main.2a7bf6f9.chunk.css"
+  },
+  {
+    "revision": "64ae6936a3ef8a7831f8",
+    "url": "/portfolio/static/js/2.d57a563c.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/portfolio/static/js/2.d57a563c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352ae0281124255c909a",
+    "url": "/portfolio/static/js/main.3853c251.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
